d01385d04281b764331b731f3cc5ffcb3e3639ba
TankEngine-ish <131184681+TankEngine-ish@users.noreply.github.com>
2024-01-14 19:08:49 +0200

    finish get planets functionality

--- README.md
+++ README.md
@@ -11,3 +11,17 @@ server.js has no express code, just setting-up the http server. Again for the pu
 4 functions in the front-end - get planets, get launches, submit launches, abort launches (once API is ready)
 
 setting CORS middleware to allow access from PORT: 3000 which is our front-end
+
+There's always a controller for a router. a router that uses a controller.
+Planets router calls functions in our planets controller. And our controller is only used by our router.
+
+We can have many models used by a single controller and a single model that is used in many different routes/controllers. We separate models because the data doesnt always match up with the
+collections, API endpoints and overall functionality that our API needs to support through our controllers and routes.
+
+node/express, nodemon, cors, react
+
+populating data on start-up.
+
+
+
+
--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,3 +1,41 @@
-const planets = [];
+const path = require ('path');
+const fs = require ('fs');
+const { parse } = require('csv-parse');
 
-module.exports = planets;
+const habitablePlanets = [];
+
+function isHabitablePlanet(planet) {
+    return planet['koi_disposition'] === 'CONFIRMED'
+        && planet ['koi_insol'] > 0.36 && planet ['koi_insol'] < 1.11
+        && planet['koi_prad'] < 1.6;
+}
+
+function loadPlanetsData() {
+    return new Promise((resolve, reject) => {
+    fs.createReadStream(path.join(__dirname, '..', '..', 'data', 'kepler_data.csv'))
+        .pipe(parse({
+            comment: '#',
+            columns: true, // this returns each row as a js object with key:value pairs
+        })) 
+        //pipe function connects a readable stream source to a writable stream destination.
+        .on('data', (data) => {
+            if (isHabitablePlanet(data)) {
+            habitablePlanets.push(data);
+            }
+        })
+        .on('error', (err) => {
+            console.log(err);
+            reject(err);
+        })
+        .on('end', () => {
+            console.log(`${habitablePlanets.length} habitable planets found!`);
+            resolve();
+        });
+    });
+}
+
+
+module.exports = {
+    loadPlanetsData,
+    planets: habitablePlanets,
+};
