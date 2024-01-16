950340198c086f130f6dfb0fb7da13cd66c1f576
TankEngine-ish <131184681+TankEngine-ish@users.noreply.github.com>
2024-01-16 16:53:13 +0200

    launches model finish

---
+++ server/src/models/launches.model.js
@@ -0,0 +1,10 @@
+const launch = {
+    flightNumber: 100,
+    mission: 'Kepler Exploration X',
+    rocket: 'Explorer IS1',
+    launchDate: new Date('december 27, 2030'),
+    destination: 'Kepler-442b',
+    customers: ['Plamen', 'Nelly'],
+    upcoming: true,
+    success: true,
+};
