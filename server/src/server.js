59f2732aa4a68a2a85be78cf094aef86ab6864b2
TankEngine-ish <131184681+TankEngine-ish@users.noreply.github.com>
2024-01-24 18:14:14 +0200

    implement new mongoose connection (no options)

--- server/src/server.js
+++ server/src/server.js
@@ -11,21 +11,23 @@ const MONG0_URL = 'mongodb+srv://dead_space_API:TSXPZE1RQR37doEb@deadspace.bgihq
 
 const server = http.createServer(app);
 
+mongoose.connection.once('open', () => {
+    console.log('MongoDB connection ready!');
+});
+
+
+mongoose.connection.on('error', (err) => {
+    console.error(err);
+});
+
 
 async function startServer () {
-    await mongoose.connect(MONG0_URL, {
-        useNewUrlParser: true,
-        useFindAndModify: false,
-        useCreateIndex: true,
-        useUnifiedTopology: true,
-    });
-
-    //these options are for mongoose to use the updated way of talking to clusters in mongodb.
+    await mongoose.connect(MONG0_URL);
     await loadPlanetsData();
-
     server.listen(PORT, () => {
-        console.log(`Listening on port ${PORT}...`);
-    });
+    console.log(`Listening on port ${PORT}...`);
+});
+
 }
 
 startServer();
