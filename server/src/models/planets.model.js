55f22547e95418ea94e41c722a37c3de2e5512d1
TankEngine-ish <131184681+TankEngine-ish@users.noreply.github.com>
2024-01-24 18:05:00 +0200

    set mongoose parameters in server.js

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -40,8 +40,6 @@ function getAllPlanets() {
 
 
 
-
-
 module.exports = {
     loadPlanetsData,
     getAllPlanets,
--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,5 @@
 const http = require ('http');
+const mongoose = require ('mongoose');
 
 const app = require ('./app');
 
@@ -6,10 +7,20 @@ const { loadPlanetsData } = require('./models/planets.model')
 
 const PORT = process.env.PORT || 8000;
 
+const MONG0_URL = 'mongodb+srv://dead_space_API:TSXPZE1RQR37doEb@deadspace.bgihqg0.mongodb.net/DeadSpace?retryWrites=true&w=majority';
+
 const server = http.createServer(app);
 
 
 async function startServer () {
+    await mongoose.connect(MONG0_URL, {
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+    });
+
+    //these options are for mongoose to use the updated way of talking to clusters in mongodb.
     await loadPlanetsData();
 
     server.listen(PORT, () => {
