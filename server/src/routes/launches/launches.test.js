0c97a629f256c3a61df83e9bd3da5e3b2454a5e1
TankEngine-ish <131184681+TankEngine-ish@users.noreply.github.com>
2024-01-27 16:26:03 +0200

    add API version to requests in JEST

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -18,7 +18,7 @@ describe ('Launches API', () => {
     describe('Test GET /launches', () => {
         test('It should respond with 200 success', async () => {
             const response = await request(app)
-                .get('/launches')
+                .get('/v1/launches')
                 .expect('Content-Type', /json/) // regex
                 .expect(200);
         });
@@ -53,7 +53,7 @@ describe ('Launches API', () => {
         
         test('It should respond with 201 created', async () => {
             const response = await request(app)
-                .post('/launches')
+                .post('/v1/launches')
                 .send(completeLaunchData)
                 .expect('Content-Type', /json/) 
                 .expect(201);
@@ -67,7 +67,7 @@ describe ('Launches API', () => {
         
         test('It should catch missing required properties', async () => {
             const response = await request(app)
-                .post('/launches')
+                .post('/v1/launches')
                 .send(launchDataWithoutDate)
                 .expect('Content-Type', /json/) 
                 .expect(400);
@@ -80,7 +80,7 @@ describe ('Launches API', () => {
         
         test('It should catch invalid dates', async () => {
             const response = await request(app)
-                .post('/launches')
+                .post('/v1/launches')
                 .send(launchDataWithInvalidDate)
                 .expect('Content-Type', /json/) 
                 .expect(400);
