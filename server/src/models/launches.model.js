a13ca3724d0c4b55d5f8c5ab1cb2e2a8ed5b7f40
TankEngine-ish <131184681+TankEngine-ish@users.noreply.github.com>
2024-02-02 00:41:47 +0200

    sort paginated data

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -5,19 +5,6 @@ const planets = require('./planets.mongo');
 
 const DEFAULT_FLIGHT_NUMBER = 100;
 
-const launch = {
-    flightNumber: 100, // flight_number
-    mission: 'Kepler Exploration X', //name
-    rocket: 'Explorer IS1',  // rocket.name
-    launchDate: new Date('December 27, 2030'), // date_local
-    target: 'Kepler-442b', // not applicable
-    customers: ['Plamen', 'Nelly'], // payload.customers for each payload
-    upcoming: true, // upcoming
-    success: true, // success
-};
-
-saveLaunch(launch);
-
 const SPACEX_API_URL = 'https://api.spacexdata.com/v5/launches/query';
 
 async function populateLaunches() {
@@ -110,6 +97,7 @@ async function getLatestFlightNumber() {
 async function getAllLaunches(skip, limit) {
     return await launchesDatabase
     .find({}, { '_id': 0, '__v': 0 })
+    .sort({ flightNumber: 1 }) // sorts paginated data
     .skip(skip)
     .limit(limit);
 }
